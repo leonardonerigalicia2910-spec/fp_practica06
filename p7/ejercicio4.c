acf3a09d4c0a46b90fe585fba6b997ee02cee82c
leonardonerigalicia2910-spec <leonardonerigalicia2910@gmail.com>
2026-03-25 14:27:23 +0000

    ejercicio4

---
+++ p7/ejercicio4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/*
+INSTRUCCIONES:
+Completa la condición:
+- Positivo si > 0
+- En otro caso imprimir "Negativo o cero"
+*/
+
+int main() {
+    int num;
+    printf("Dime un numero"); 
+       scanf("%d", &num);
+
+    if (num > 0) {
+        printf("Positivo\n");
+    } else {
+        printf("Negativo o cero\n");
+    }
+printf("arriba las chivas"); 
+   return 0;
+}
