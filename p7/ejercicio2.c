487d40df0ad213e03c24937f516c6dc3e70054ac
leonardonerigalicia2910-spec <leonardonerigalicia2910@gmail.com>
2026-03-25 14:05:28 +0000

    ejercicio2

---
+++ p7/ejercicio2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/*
+INSTRUCCIONES:
+Completa la condición para que imprima "Mayor de edad"
+si la edad es 18 o más.
+*/
+
+int main() {
+    int edad;
+    printf ("ingresa tu edad:");
+        scanf("%d", &edad);
+
+    if (edad > 18) {
+        printf("Mayor de edad\n");
+    }
+
+    return 0;
+}
